1c975c4570b377aced3ec16e7e10515e92c0cb74
Olga <olga.polska33@gmail.com>
2025-11-14 07:51:05 +0100

    Adding show available flights function for user

---
+++ functions/flights.py
@@ -0,0 +1,22 @@
+from utils.file_handler import load_data, save_data
+from classes.airplane import Airplane
+from classes.flight import Flight
+
+def load_airplanes(file_path):
+    airplanes = []
+    data = load_data(file_path)
+    for row in data:
+        id,name,model,capacity,distance = row
+        airplanes.append(Airplane(int(id),name,model,int(capacity), int(distance.strip())))
+    return airplanes
+                
+        
+def load_flights(file_path_flights, file_path_airplanes):
+    flights = []
+    airplanes = load_airplanes(file_path_airplanes)
+    data = load_data(file_path_flights)
+    for row in data:
+        idFlight, idAirplane, origin, destination, distance, duration, ticketPrice, points = row
+        airplane = next((a for a in airplanes if a.id == int(idAirplane)),None)
+        flights.append(Flight(int(idFlight), airplane, origin, destination, float(distance), float(duration), float(ticketPrice), int(points.strip())))
+    return flights
