ce2117657bfe39c0c2affa81c8edb55ec53f4f4a
Olga <olga.polska33@gmail.com>
2025-11-17 18:55:08 +0100

    function available flights updated, function timetable add

--- classes/flight.py
+++ classes/flight.py
@@ -1,5 +1,5 @@
 class Flight:
-    def __init__(self, id ,airplane, origin, destination, distance, duration, ticketPrice, points):
+    def __init__(self, id ,airplane, origin, destination, distance, duration, ticketPrice, points, takenSeats, departure):
         self.id = id
         self.airplane = airplane #object of airplane
         self.origin = origin
@@ -8,10 +8,12 @@ class Flight:
         self.duration = duration  #in hours
         self.ticketPrice = ticketPrice
         self.points = points
+        self.takenSeats = takenSeats
+        self.departure = departure #datetime iso
 
     def __str__(self):
-        return (f"Flight {self.origin} -> {self.destination}, distance: {self.distance} km, duration: {self.duration} h, \nprice: {self.ticketPrice}, points: {self.points}, airplane: {self.airplane.name} {self.airplane.model}")
+        return (f"Flight {self.origin} -> {self.destination}, departure: {self.departure.strftime('%Y-%m-%d %H:%M')}, distance: {self.distance} km, duration: {self.duration} h, \nprice: {self.ticketPrice}, points: {self.points}, airplane: {self.airplane.name} {self.airplane.model}, seats {self.takenSeats}/{self.airplane.seatsQuantity}")
         
     def ShowInformation(self):
-        print((f"Flight {self.origin} -> {self.destination}, distance: {self.distance} km, duration: {self.duration} h, \nprice: {self.ticketPrice}, points: {self.points}, airplane: {self.airplane.name} {self.airplane.model}"))
+        print((f"Flight {self.origin} -> {self.destination}, departure: {self.departure.strftime('%Y-%m-%d %H:%M')}, distance: {self.distance} km, duration: {self.duration} h, \nprice: {self.ticketPrice}, points: {self.points}, airplane: {self.airplane.name} {self.airplane.model}, seats {self.takenSeats}/{self.airplane.seatsQuantity}"))
         
